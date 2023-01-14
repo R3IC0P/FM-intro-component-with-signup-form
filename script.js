b66987c4258e45a642ef76c5d4cb4cd42dff45d5
R3IC0P <luxorpol@gmail.com>
2023-01-14 18:50:18 +0100

    repaired class naming convention

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.forms[0].addEventListener("submit", (e) => {
+document.querySelector(".js-intro-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    const inputs = document.querySelectorAll(".intro__sign-up--form input");
+    const inputs = document.querySelectorAll(".intro__form-item > input");
     inputs.forEach((input) => {
         input.classList.remove("is-invalid");
         if (input.value.length === 0) {
